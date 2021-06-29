22bec7bd4a764bea9d72be4be6fc6a7180f1dfdd
lizhengxin <lizhengxin.lzx@bytedance.com>
2021-06-29 11:10:30 +0800

    26进制问题

---
+++ code/src/main/java/com/lzx/leetCode/mod.java
@@ -0,0 +1,33 @@
+package com.lzx.leetCode;
+/**
+ * @author lizhengxin<lizhengxin.lzx @ bytedance.com>
+ * @date 06/29/2021 10:45 上午
+ **/
+
+import com.sun.xml.internal.xsom.impl.parser.SubstGroupBaseTypeRef;
+
+/**
+ * *****************************************************
+ * Copyright (C) 2021 bytedance.com. All Rights Reserved
+ * This file is part of bytedance EA project.
+ * Unauthorized copy of this file, via any medium is strictly prohibited.
+ * Proprietary and Confidential.
+ * ****************************************************
+ **/
+class mod {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+        while (columnNumber > 0) {
+            int a0 = (columnNumber - 1) % 26 + 1;
+            sb.append((char)(a0 - 1 + 'A'));
+            columnNumber = (columnNumber - a0) / 26;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        int col = 26;
+        System.out.println(new mod().convertToTitle(col));
+    }
+
+}
